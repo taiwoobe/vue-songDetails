7f7a1b5d1df18c9f49c342a8e284778a23cf96e9
Taiwo Ibidapo-Obe <Ibidapo-Obe>
2018-06-08 16:51:37 +0100

    Installed Express Server

---
+++ server-side/src/app.js
@@ -0,0 +1,20 @@
+console.log('First Console Log')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'Hello World!!',
+        status: 'active'
+    })
+})
+
+app.listen(process.env.PORT || 8081)
